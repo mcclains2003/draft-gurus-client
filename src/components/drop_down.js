d12820d8a460c5ce699822cfc0b7acbb3c4d72e1
mcclains2003 <mcclains2003@gmail.com>
2018-06-22 09:18:00 -0500

    Created a dropdown comp - setting it up for reuse with multi uses.

---
+++ src/components/drop_down.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class DropDown extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = { year: '' };
+    }
+
+    render() {
+        return (
+            <div>
+                <select onChange={ event => console.log(event.target.value) }>
+                    <option value="default">Select a Draft Year</option>
+                    <option value="2018">2018</option>
+                    <option value="2019">2019</option>
+                    <option value="2020">2020</option>
+                    <option value="2021">2021</option>
+                </select>
+            </div> 
+        )
+    }
+
+    // onOptionChange(event) {
+    //     console.log(event.target.value);
+    // }
+}
+
+// class MenuTagItems extends Component{
+//     render() {
+//         var years = [2018, 2019, 2020, 2021]
+//         var dropDownList = years.map(function(year){
+//             return <MenuItem>{ year }</MenuItem>
+//         })
+
+//         return { dropDownList }
+//     }
+// }
+
+// MenuTagItems = () => {
+//     years.forEach(function(year) {
+//         return <MenuItem>year</MenuItem>
+//     })
+// }
+
+export default DropDown;
