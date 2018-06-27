09ed9736d0433c81ff1687e1beb585539b747c01
mcclains2003 <mcclains2003@gmail.com>
2018-06-27 12:26:44 -0500

    Preliminary test of passing years manually to dropdown - working

--- src/components/draft_year_drop_down.js
+++ src/components/draft_year_drop_down.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 
-import DropDown from './components/drop_down';
+import DropDown from './drop_down';
 
 const DraftYearDropDown = () => {
     var yearArray = ['2017', '2018', '2019', '2020']
     return <div>
         <h1>Please Select the Draft Year</h1>
-        <DropDown title="Choose Draft Year" data= yearArray />
+        <DropDown data={yearArray} />
     </div>;
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+export default DraftYearDropDown;
+// ReactDOM.render(<App />, document.querySelector('#root'));
