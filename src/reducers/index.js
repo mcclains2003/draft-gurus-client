59d7baef50ce7f8aab6ba6beee7325f31b265285
mcclains2003 <mcclains2003@gmail.com>
2018-06-27 14:30:40 -0500

    wired up draft years reducer in the index file, no api data yet

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import YearsReducer from './reducer_years';
 
 const rootReducer = combineReducers({
-    
+    years: YearsReducer
 });
 
 export default rootReducer;
