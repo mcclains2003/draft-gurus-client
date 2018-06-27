d644ea844d3e2b17c3d0315d804f4cb74a981364
mcclains2003 <mcclains2003@gmail.com>
2018-06-27 12:28:00 -0500

    Made dropdown to accept an array, working but changes needed to make dry

--- src/components/drop_down.js
+++ src/components/drop_down.js
@@ -4,20 +4,21 @@ class DropDown extends Component{
     constructor(props) {
         super(props);
 
-        this.state = { year: '' };
+        this.state = { years: this.props.data };
     }
 
     render() {
+        var selectionOptions = this.state.years.map(function(selection){
+            return <option value={selection}>{selection}</option>
+        })
+
         return (
             <div>
                 <select 
                     value={ this.state.year }
                     onChange={ event => this.setState({ year: event.target.value }) }>
                     <option value="default">Make a Selection</option>
-                    <option value="2018">2018</option>
-                    <option value="2019">2019</option>
-                    <option value="2020">2020</option>
-                    <option value="2021">2021</option>
+                    { selectionOptions }
                 </select>
             </div> 
         );
