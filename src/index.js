7d91a23c171a2ac4b75ad58e5793413d991a2944
mcclains2003 <mcclains2003@gmail.com>
2018-06-27 12:11:59 -0500

    Changed App to a class and called draft year drop down.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-import DropDown from './components/drop_down';
+import DraftYearDropDown from './components/draft_year_drop_down';
 
-const App = () => {
-    return <div>
-        <DropDown title="Choose Draft Year"/>
-    </div>;
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { years: [] };
+    }
+
+    render() {
+        return (
+            <div>
+                <DraftYearDropDown title="Choose Draft Year"/>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
