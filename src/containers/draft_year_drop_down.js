423d1d2e786a7938686db47082bc0424af4f3821
mcclains2003 <mcclains2003@gmail.com>
2018-06-28 11:13:37 -0500

    changed dydd from comp to container - wired it up to redux for store

--- src/containers/draft_year_drop_down.js
+++ src/containers/draft_year_drop_down.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import DropDown from '../components/drop_down';
 
-export default class DraftYearDropDown extends Component {
+class DraftYearDropDown extends Component {
     render() {
-        var yearArray = ['2017', '2018', '2019', '2020'];
-
         return (
             <div>
                 <h1>Please Select the Draft Year</h1>
-                <DropDown data={yearArray} />
+                <DropDown data={this.props.years} />
             </div>
         );
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        years: state.years
+    }
+}
+
+export default connect(mapStateToProps)(DraftYearDropDown);
